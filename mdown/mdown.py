564f8eb75e765ce4e2e2c8757e8ee0fe0d95b570
Tanyiqu <1953649096@qq.com>
2020-11-01 11:17:18 +0800

    检测主函数的参数

--- mdown/mdown.py
+++ mdown/mdown.py
@@ -11,50 +11,87 @@ parser.add_argument("url", nargs="*", help="url of m3u8 video, default argument,
 parser.add_argument('-v', '--version', action='version', version=R.string.VERSION_NAME)
 
 parser.add_argument('-u', '--url', metavar='', help='to specify m3u8 video url')
-parser.add_argument('-n', '--name', metavar='', help='name of file', default='')
-parser.add_argument('-t', '--thread', metavar='', type=int, help='threads of download progress')
-parser.add_argument('-C', metavar='', help='download location')
+parser.add_argument('-n', '--name', metavar='', help='name of file')
+parser.add_argument('-t', '--thread', metavar='', type=int, help='threads of download progress', default=16)
+parser.add_argument('-C', metavar='', help='absolute path of file')
+
+
+# 检测传入的参数，返回字典形式的参数列表，如果有检测失败的则返回None
+def analyseArgs(args):
+    dit = {
+        'url': args.url[0],
+        'name': args.name,
+        'thread': args.thread,
+        'path': args.C
+    }
+
+    # 简单判断一下url
+    if not TextUrl.isUrl(dit['url']):
+        print('error: "%s" is not a correct url' % dit['url'])
+        return None
+
+    # 名字
+    if dit['name'] is None:
+        dit['name'] = 'index.ts'
+        pass
+
+    # 路径
+    if dit['path'] is None:
+        dit['path'] = '.'
+        pass
+    elif not TextUrl.isPath(dit['path']):
+        print('error: "%s" is not a correct path' % dit['path'])
+        return None
+        pass
+
+    # 线程数
+    if not 0 < dit['thread'] < 33:
+        print('error: the number of threads must be greater than 0 and less than or equal to 32')
+        return None
+
+    return dit
 
 
 def main():
     # 接收系统参数
     args = parser.parse_args()
+
     # 判断有没有传url
     if len(args.url) < 1:
         print('error: the following arguments are required: url')
         return
 
-    url = args.url[0]
+    # 检测参数
+    args = analyseArgs(args)
+    if args is None:
+        return
 
     # ------------------------
     # print(args)
     # print(url)
     # ------------------------
 
-    # 简单判断一下url
-    if not TextUrl.isUrl(url):
-        print('error: "%s" is not a correct url' % url)
-        return
-
+    # 开始
     print()
     print('downloading with mdown...')
-    print('url : ' + url)
+    print('url : ' + args['url'])
     print()
+
     # 构造M3u8
     print('parsing m3u8...')
-    m3u8 = M3u8(url)
-
+    m3u8 = M3u8(args['url'])
     if not m3u8.isM3u8():
-        print('error: "%s" is not a correct url' % url)
+        print('error: "%s" is not a correct url' % args['url'])
         pass
-
-    print()
     print('parsing succeed')
-    print('Length:\t\t%s' % m3u8.getTsLength())
-    print('Duration:\t%s' % m3u8.getDuration())
+    print()
+    print('%s%-16s%s' % ('    ', 'Length:', m3u8.getTsLength()))
+    print('%s%-16s%s' % ('    ', 'Duration:', m3u8.getDuration()))
+    print()
 
     # 构造M3u8Downloader
-    downloader = M3u8Downloader(m3u8.getList())
+    downloader = M3u8Downloader(m3u8.getList(), args['path'], args['name'])
+    print('downloading...')
     downloader.download()
 
     pass
