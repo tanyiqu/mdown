c6ba542e10b3b69ad496212c57c6772d9cb66f12
Tanyiqu <1953649096@qq.com>
2020-11-01 11:16:39 +0800

    判断路径是否存在的工具函数

--- mdown/util/TextUrl.py
+++ mdown/util/TextUrl.py
@@ -1,6 +1,7 @@
 """
 字符串处理工具类
 """
+from pathlib import Path
 
 
 # 判断一个字符串是不是url
@@ -12,6 +13,12 @@ def isUrl(url: str):
     """
     return url.startswith('http')
     pass
+
+
+# 判断给定路径的目录是否存在
+def isPath(path: str):
+    my_file = Path(path)
+    return my_file.is_dir()
 
 
 if __name__ == '__main__':
