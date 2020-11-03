7658d9a1aebb0d61559de191a43af4a427b96f6a
Tanyiqu <1953649096@qq.com>
2020-11-03 21:40:18 +0800

    改进下载ts文件的算法

--- mdown/util/DownloadUtil.py
+++ mdown/util/DownloadUtil.py
@@ -9,6 +9,51 @@ import threading
 import threadpool
 
 
+# class M3u8Downloader:
+#     pass
+
+
+class TsDownloader:
+    """
+    下载ts文件的下载器
+    """
+
+    path = 'path'
+    __path = '__path'
+
+    def __init__(self):
+        pass
+
+    def print(self):
+        print(self.path)
+        print(self.__path)
+        print(TsDownloader.path)
+        print(TsDownloader.__path)
+        pass
+
+    def set(self):
+        self.path = 'p'
+        self.__path = '__p'
+        pass
+
+    # def __init__(self, url, path, name):
+    #     # self.__url = url
+    #     # self.__path = path
+    #     # self.__name = name
+    #     pass
+
+    pass
+
+
+if __name__ == '__main__':
+    ts = TsDownloader()
+    ts.print()
+    ts.set()
+    ts.print()
+
+    pass
+
+
 class M3u8Downloader:
     __list = []
     __filename = ''
@@ -88,22 +133,37 @@ class Downloader:
         #     pass
 
         # 下完再写方式
-        resp = requests.get(self.__url)
-        with open(self.__path + '/' + self.__filename, "wb") as f:
-            f.write(resp.content)
+        # resp = requests.get(self.__url)
+        # with open(self.__path + '/' + self.__filename, "wb") as f:
+        #     f.write(resp.content)
+        #     pass
+        # #
+        # print('%s is ok' % self.__num)
+        #
+        i = 0
+        # 如果请求失败就重新请求，直至请求成功
+        while True:
+            try:
+                resp = requests.get(url=self.__url, timeout=5)
+                with open(self.__path + '/' + self.__filename, "wb") as f:
+                    f.write(resp.content)
+                    pass
+                print('%s is ok' % self.__num)
+                break
+                pass
+            except requests.exceptions.RequestException:
+                i += 1
+                print('%s retry %d' % (self.__num, i))
+                pass
             pass
-        #
-        print('%s is ok' % self.__num)
-        #
         pass
 
     pass
 
-
-if __name__ == '__main__':
-    d = Download('https://yuledy.helanzuida.com/20200402/1745_5f787176/1000k/hls/964e77c2ad0000033.ts',
-                 # d = Download('https://mirrors.tuna.tsinghua.edu.cn/qt/archive/qt/5.12/5.12.8/qt-opensource-linux-x64-5.12.8.run',
-                 'C:/Users/Tanyiqu/Desktop/测试打包',
-                 '4555.ts')
-    print(d.download())
-    pass
+# if __name__ == '__main__':
+#     d = Download('https://yuledy.helanzuida.com/20200402/1745_5f787176/1000k/hls/964e77c2ad0000033.ts',
+#                  # d = Download('https://mirrors.tuna.tsinghua.edu.cn/qt/archive/qt/5.12/5.12.8/qt-opensource-linux-x64-5.12.8.run',
+#                  'C:/Users/Tanyiqu/Desktop/测试打包',
+#                  '4555.ts')
+#     print(d.download())
+#     pass
